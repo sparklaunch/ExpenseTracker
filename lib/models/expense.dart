3afed4237527c99c972f392fde68877061efa481
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-08-14 08:02:54 +0900

    Update expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,11 +2,23 @@ import 'package:uuid/uuid.dart';
 
 const Uuid uuid = Uuid();
 
+enum Category {
+  food,
+  leisure,
+  travel,
+  work;
+}
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  Expense({required this.title, required this.amount, required this.date})
+  final Category category;
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4();
 }
