17f675f963f1765c02099600c58b0fd6ccedbdc8
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-15 09:30:57 +0900

    Update

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
 const Uuid uuid = Uuid();
+
+final DateFormat dateFormatter = DateFormat.yMd();
 
 enum Category {
   food,
@@ -21,4 +32,7 @@ class Expense {
       required this.date,
       required this.category})
       : id = uuid.v4();
+  String get formattedDate {
+    return dateFormatter.format(date);
+  }
 }
