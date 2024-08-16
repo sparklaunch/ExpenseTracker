df658e43d5b5de7f6adcb2c596630a63af2b3d84
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-16 15:21:35 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: colorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: colorScheme.onPrimaryContainer,
+            foregroundColor: colorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: colorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: colorScheme.primaryContainer),
+          ),
+          textTheme: const TextTheme()),
+      home: const Expenses(),
     ),
   );
 }
+
+final ColorScheme colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -25,6 +25,15 @@ class _ExpensesState extends State<Expenses> {
   ];
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found. Start adding some!"),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Expense Tracker"),
@@ -36,15 +45,7 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: Column(
-        children: [
-          const Text("Chart"),
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
-          )
-        ],
+        children: [const Text("Chart"), Expanded(child: mainContent)],
       ),
     );
   }
@@ -65,8 +66,26 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final int expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense deleted"),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(
+              () {
+                _registeredExpenses.insert(expenseIndex, expense);
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
